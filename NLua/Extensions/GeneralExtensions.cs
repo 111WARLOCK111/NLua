6929758ff2556ccd16b2e2573de88fd9befb2c84
Ali Deym <warlock.killer.gm@gmail.com>
2014-08-02 16:57:42 +0430

    Fixed projects + Merged glua bridge.

---
+++ NLua/Extensions/GeneralExtensions.cs
@@ -0,0 +1,166 @@
+/*
+ * This file is part of NLua.
+ * 
+ * Copyright (c) 2013 Vinicius Jarina (viniciusjarina@gmail.com)
+ * Copyright (C) 2012 Megax <http://megax.yeahunter.hu/>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace NLua.Extensions
+{
+	/// <summary>
+	/// Some random extension stuff.
+	/// </summary>
+	static class CheckNull
+	{
+		/// <summary>
+		/// Determines whether the specified obj is null.
+		/// </summary>
+		/// <param name="obj">The obj.</param>
+		/// <returns>
+		/// 	<c>true</c> if the specified obj is null; otherwise, <c>false</c>.
+		/// </returns>
+		/// 
+		
+#if USE_KOPILUA
+		public static bool IsNull (object obj)
+		{
+			return (obj == null);
+		}
+#else
+
+		public static bool IsNull (IntPtr ptr)
+		{
+			return (ptr.Equals (IntPtr.Zero));
+		}
+#endif
+	}
+
+	static class TypeExtensions
+	{
+		public static bool HasAdditionOpertator (this Type t)
+		{
+			if (t.IsPrimitive) 
+				return true;
+
+			var op_add = t.GetMethod ("op_Addition");
+			return op_add != null;  
+		}
+
+		public static bool HasSubtractionOpertator (this Type t)
+		{
+			if (t.IsPrimitive)
+				return true;
+
+			var op_add = t.GetMethod ("op_Subtraction");
+			return op_add != null;
+		}
+
+		public static bool HasMultiplyOpertator (this Type t)
+		{
+			if (t.IsPrimitive)
+				return true;
+
+			var op_add = t.GetMethod ("op_Multiply");
+			return op_add != null;
+		}
+
+		public static bool HasDivisionOpertator (this Type t)
+		{
+			if (t.IsPrimitive)
+				return true;
+
+			var op_add = t.GetMethod ("op_Division");
+			return op_add != null;
+		}
+
+		public static bool HasModulusOpertator (this Type t)
+		{
+			if (t.IsPrimitive)
+				return true;
+
+			var op_add = t.GetMethod ("op_Modulus");
+			return op_add != null;
+		}
+
+		public static bool HasUnaryNegationOpertator (this Type t)
+		{
+			if (t.IsPrimitive)
+				return true;
+
+			var op_add = t.GetMethod ("op_UnaryNegation");
+			return op_add != null;
+		}
+
+		public static bool HasEqualityOpertator (this Type t)
+		{
+			if (t.IsPrimitive)
+				return true;
+
+			var op_add = t.GetMethod ("op_Equality");
+			return op_add != null;
+		}
+
+		public static bool HasLessThanOpertator (this Type t)
+		{
+			if (t.IsPrimitive)
+				return true;
+
+			var op_add = t.GetMethod ("op_LessThan");
+			return op_add != null;
+		}
+
+		public static bool HasLessThanOrEqualOpertator (this Type t)
+		{
+			if (t.IsPrimitive)
+				return true;
+
+			var op_add = t.GetMethod ("op_LessThanOrEqual");
+			return op_add != null;
+		}
+	}
+
+	static class StringExtensions
+	{
+		public static IEnumerable<string> SplitWithEscape (this string input, char separator, char escapeCharacter)
+		{
+			int start = 0;
+			int index = 0;
+			while (index < input.Length) {
+				index = input.IndexOf (separator, index);
+				if (index == -1)
+					break;
+
+				if (input [index - 1] == escapeCharacter) {
+					input = input.Remove (index - 1, 1);
+					continue;
+				}
+
+
+				yield return input.Substring (start, index - start);
+				index++;
+				start = index;
+			}
+			yield return input.Substring (start);
+		}
+	}
+}
